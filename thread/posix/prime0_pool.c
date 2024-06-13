981f62645d4042a08f09c11c7341bc71bb380853
io.ing <io.ing@outlook.com>
2024-06-14 00:57:34 +0800

    pool

---
+++ thread/posix/prime0_pool.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#define LEFT 3000000
+#define RIGHT 3000200
+#define THRNUM 4
+
+static int num = 0;
+static pthread_mutex_t mut_num = PTHREAD_MUTEX_INITIALIZER;
+
+int is_primer(int num)
+{
+    int i;
+
+    for (i=2; i<num; ++i)
+    {
+        if (num % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+void *thr_prime(void *p)
+{
+    int i;
+
+    while (1)
+    {
+        pthread_mutex_lock(&mut_num);
+        while (num == 0)
+        {
+            pthread_mutex_unlock(&mut_num);
+            sched_yield();
+            pthread_mutex_lock(&mut_num);
+        }
+        if (num == -1)
+        {
+            pthread_mutex_unlock(&mut_num);
+            break;
+        }
+        i = num;
+        num = 0;
+        pthread_mutex_unlock(&mut_num);
+
+        if (is_primer(i))
+        {
+            printf("[%d]: %d is primer\n", (int)p, i);
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    int i;
+    int err;
+    pthread_t tid[THRNUM];
+
+    for (i=0; i<THRNUM; ++i)
+    {
+        err = pthread_create(tid+i, NULL, thr_prime, (void *)i);
+        if (err < 0)
+        {
+            /* 注意：标准的处理方法需要把之前创建的线程都释放 */
+            fprintf(stderr, "pthread_create(): %s\n", strerror(err));
+            exit(1);
+        }
+    }
+
+    for (i=LEFT; i<RIGHT; i++)
+    {
+        pthread_mutex_lock(&mut_num);
+        while (num != 0)
+        {
+            pthread_mutex_unlock(&mut_num);
+            sched_yield();
+            pthread_mutex_lock(&mut_num);
+        }
+        num = i;
+        pthread_mutex_unlock(&mut_num);
+    }
+
+    pthread_mutex_lock(&mut_num);
+    while (num != 0)
+    {
+        pthread_mutex_unlock(&mut_num);
+        sched_yield();
+        pthread_mutex_lock(&mut_num);
+    }
+    num = -1;
+    pthread_mutex_unlock(&mut_num);
+
+    /* 如果不使用数组的话，tid 只会保存一份无法全部释放 */
+    for (i=0; i<THRNUM; i++)
+        pthread_join(tid[i], NULL);
+
+    pthread_mutex_destroy(&mut_num);
+    exit(0);
+}
