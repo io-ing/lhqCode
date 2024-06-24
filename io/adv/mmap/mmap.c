1596ef68f455387f2c12bc21ce470cb337345458
lzl <zhuolin.li@fanvil.com>
2024-06-24 20:42:13 +0800

    mmap

--- io/adv/mmap/mmap.c
+++ io/adv/mmap/mmap.c
@@ -10,7 +10,9 @@ int main(int argc, char **argv)
 {
     int fd;
     struct stat statres;
+    /* 不一定用 void * 类型，定义为最方便的类型 */
     char *str;
+    /* 注意：在当前测试环境下 i 的类型为 off_t 程序将发生异常，为什么？*/
     int i;
     /* 注意：没有在此程序中考虑溢出 */
     int count;
@@ -30,14 +32,17 @@ int main(int argc, char **argv)
 
     if (fstat(fd, &statres) < 0)
     {
-        perror("stat()");
+        perror("fstat()");
+        close(fd);
         exit(1);
     }
 
+    /* 用 MAP_SHARED 或 MAP_PRIVATE 都可以 */
     str = mmap(NULL, statres.st_size, PROT_READ, MAP_SHARED, fd, 0);
     if (str == MAP_FAILED)
     {
         perror("mmap()");
+        close(fd);
         exit(1);
     }
     /* 不用写在最后 */
@@ -50,7 +55,11 @@ int main(int argc, char **argv)
     }
     fprintf(stdout, "%d\n", count);
 
-    munmap(str, statres.st_size);
+    if (munmap(str, statres.st_size) < 0)
+    {
+        perror("munmap()");
+        exit(1);
+    }
 
     exit(0);
 }
