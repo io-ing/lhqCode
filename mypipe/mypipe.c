2e618eed42cff4748a2bd62155aa404856668132
lzl <zhuolin.li@fanvil.com>
2024-06-28 19:04:37 +0800

    mypipe

--- mypipe/mypipe.c
+++ mypipe/mypipe.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#include "mypipe.h"
+
 #define PIPESIZE 1024
 
 struct mypipe_st
 {
     int head;
     int tail;
-// data datasize
-    char pipe[PIPESIZE];
-    int pipesize;
-// count_rd;
-// count_wr;
-    int writeCount;
-    int readCount;
+    char data[PIPESIZE];
+    int datasize;
+    int count_rd;
+    int count_wr;
     pthread_mutex_t mut;
     pthread_cond_t cond;
 };
@@ -30,44 +29,54 @@ mypipe_t *mypipe_init()
     me->head = 0;
     me->tail = 0;
     /* 注意：data 不用初始化，data 是一个数组，把他初始化为什么都不算特殊 */
-    me->pipesize = 0;
-    me->writeCount = 0;
-    me->readCount = 0;
-    pthread_mutex_init(&mut, NULL);
-    pthread_cond_init(&cond, NULL);
+    me->datasize = 0;
+    me->count_rd = 0;
+    me->count_wr = 0;
+    pthread_mutex_init(&me->mut, NULL);
+    pthread_cond_init(&me->cond, NULL);
 
     return me;
+}
+
+static int next(int num)
+{
+    return (num+1) % PIPESIZE;
 }
 
 static int mypipe_readbyte_unlocked(struct mypipe_st *me, char *datap)
 {
     *datap = me->data[me->head];
     me->head = next(me->head);
-    me->pipesize--;
+    me->datasize--;
     return 0;
 }
 
-int mypipe_read(mypipe_t *ptr, void *buff, size_t count)
+int mypipe_read(mypipe_t *ptr, char *buff, size_t count)
 {
     struct mypipe_st *me = ptr;
     int i;
 
     pthread_mutex_lock(&me->mut);
     /* 当管道中没有数据但是有读者才会等待，也就是说管道中的数据和写着发生改变才会继续 */
-    while (me->pipesize<=0 && me->writeCount>0)
-        pthread_cond_wait(&me->conf, &me->mut);
+    while (me->datasize<=0 && me->count_wr>0)
+        pthread_cond_wait(&me->cond, &me->mut);
+
+    if (me->datasize<=0 && me->count_wr<=0)
+    {
+        pthread_mutex_unlock(&me->mut);
+        return 0;
+    }
 
     for (i=0; i<count; i++)
     {
-        if (me->pipesize <= 0)
+        if (me->datasize <= 0)
             break;
 #if 0
             mypipe_readbyte_unlocked(me, buf+1);
 #else
-        buff[i] = me->pipe[me->head];
-        //strncpy(buff, me->pipe[me->head], 1);
+        buff[i] = me->data[me->head];
         me->head = (me->head+1) % PIPESIZE;
-        me->pipesize--;
+        me->datasize--;
 #endif
     }
 
@@ -78,30 +87,31 @@ int mypipe_read(mypipe_t *ptr, void *buff, size_t count)
     return i;
 }
 
-int mypipe_write(mypipe_t *ptr, const void *, size_t count)
+int mypipe_write(mypipe_t *ptr, const char *buff, size_t count)
 {
     struct mypipe_st *me = ptr;
+    int i;
 
     pthread_mutex_lock(&me->mut);
-    while (me->pipesize>=PIPESIZE || me->readCount<=0)
+    while (me->datasize>=PIPESIZE && me->count_rd>0)
         pthread_cond_wait(&me->cond, &me->mut);
 
-    if (me->pipesize<=0 && me->readCount<=0)
+    if (me->datasize>=PIPESIZE && me->count_rd<=0)
     {
         pthread_mutex_unlock(&me->mut);
         return 0;
     }
-    while (i=0; i<count; i++)
+
+    for (i=0; i<count; i++)
     {
-        if (me->pipesize >= PIPESIZE)
+        if (me->datasize >= PIPESIZE)
             break;
-        memcpy(me->pipe[i], buff[i], 1);
+        me->data[me->tail] = buff[i];
         me->tail = (me->tail+1) % PIPESIZE;
-        me->pipesize++;
+        me->datasize++;
     }
 
     pthread_cond_broadcast(&me->cond);
-
     pthread_mutex_unlock(&me->mut);
 
     return i;
@@ -111,8 +121,8 @@ int mypipe_destroy(mypipe_t *ptr)
 {
     struct mypipe_st *me = ptr;
 
-    pthread_mutex_destroy(me->mutex);
-    pthread_cond_destroy(me->cond);
+    pthread_mutex_destroy(&me->mut);
+    pthread_cond_destroy(&me->cond);
     free(me);
 
     return 0;
@@ -124,13 +134,13 @@ int mypipe_register(mypipe_t *ptr, int opmap)
 
     pthread_mutex_lock(&me->mut);
     if (opmap & MYPIPE_READ)
-        me->readCount++;
+        me->count_wr++;
 
     if (opmap & MYPIPE_WRITE)
-        me->writeCount++;
+        me->count_rd++;
 
     /* 确保所有的管道都有一个读者和一个写者 */
-    for (me->readCount<=0 || me->writeCount<=0)
+    while (me->count_wr<=0 || me->count_rd<=0)
         pthread_cond_wait(&me->cond, &me->mut);
 
     /* 读者/写者发信号告诉阻塞在这里的写者/读者，写在上面/下面都可以 */
@@ -148,14 +158,14 @@ int mypipe_unregister(mypipe_t *ptr, int opmap)
     /* if error */
 
     pthread_mutex_lock(&me->mut);
-    if (opmap & 1)
-        me->readCount--;
+    if (opmap & MYPIPE_READ)
+        me->count_wr--;
 
-    if (opmap & 2)
-        me->writeCount--;
+    if (opmap & MYPIPE_WRITE)
+        me->count_rd--;
 
     /* 可能有多个阻塞在 read 中，需要唤醒 */
-    pthread_cond_broadcast(&me->cond, &me->mut);
+    pthread_cond_broadcast(&me->cond);
     pthread_mutex_unlock(&me->mut);
 
     return 0;
--- mypipe/mypipe.h
+++ mypipe/mypipe.h
@@ -1,6 +1,8 @@
 #ifndef MY_PIPE__
 #define MY_PIPE__
 
+#include <stddef.h>
+
 #define MYPIPE_READ  0x00000001UL
 #define MYPIPE_WRITE 0x00000002UL
 
@@ -8,9 +10,9 @@ typedef void mypipe_t;
 
 mypipe_t *mypipe_init();
 
-int mypipe_read(mypipe_t *, void *, size_t);
+int mypipe_read(mypipe_t *, char *, size_t);
 
-int mypipe_write(mypipe_t *, const void *, size_t);
+int mypipe_write(mypipe_t *, const char *, size_t);
 
 int mypipe_destroy(mypipe_t *);
 
