95c4043b9f70cf082e9fda0cb174be155ee1ba38
lzl <zhuolin.li@fanvil.com>
2024-09-03 20:30:48 +0800

    proj server

--- proj/net_radio/src/server/server.c
+++ proj/net_radio/src/server/server.c
@@ -3,6 +3,26 @@
 
 #include <unistd.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <syslog.h>
+
+#include <string.h>
+
+#include <signal.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <netinet/ip.h>
+
+#include <arpa/inet.h>
+
+#include <net/if.h>
+
 #include "proto.h"
 
 #define DEFAULT_MEDIA_DIR   "/var/media"
@@ -17,8 +37,8 @@ enum
 struct server_conf_st
 {
     char *recv_port;
-    char *multicast_group
-        char *media_dir;
+    char *multicast_group;
+    char *media_dir;
     char *interface;
     int run_mode;
 }
@@ -31,18 +51,6 @@ struct server_conf_st server_conf =
     .interface = DEFAULT_INTERFACE,
     .run_mode = RUN_DAEMON,
 };
-
-static void print_help()
-{
-    printf("Usage: server [options]\n");
-    printf("Options:\n");
-    printf(" -M, --multicast-group  Set multicast group\n");
-    printf(" -P, --port             Set receive port\n");
-    printf(" -d, --media-dir        Set media directory\n");
-    printf(" -i, --interface        Set network interface\n");
-    printf(" -h, --help             Show help\n");
-    printf(" --foreground           Run in foreground\n");
-}
 
 static void print_help()
 {
@@ -101,16 +109,38 @@ static void parse_command_line_options(int argc, char *argv[])
     }
 }
 
-static void demonize()
+
+static void handle_signal(int sig)
+{
+    syslog(LOG_INFO, "Received signal %d, exiting...", sig);
+    closelog();
+    exit(0);
+}
+
+static void setup_signals()
+{
+    struct sigaction sa;
+
+    sa.sa_handler = demon_exit;
+    sigemptyset(&sa.sa_flags);
+    sa.sa_flags = 0;
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
+}
+
+static int demonize()
 {
     pid_t pid;
+    int fd;
 
     /* 1. 调用 **`fork()`** 创建子进程，父进程退出 */
     pid = fork();
     if (pid < 0)
     {
-        perror("fork()");
-        exit(1);
+        syslog(LOG_ERR, "fork() %s", strerror(errno));
+        return -1;
     }
 
     if (pid > 0)
@@ -118,30 +148,89 @@ static void demonize()
         exit(0);
     }
 
+    fd = open("/dev/null", O_RDWR);
+    if (fd < 0)
+    {
+        syslog(LOG_ERR, "open() %s", strerror(errno));
+        return -1;
+    }
+
+    /* 4. 重定向标准输入、输出和错误到 **`/dev/null`** 或其他地方 */
+    dup2(fd, 0);
+    dup2(fd, 1);
+    dup2(fd, 2);
+    if (fd > 2)
+    {
+        close(fd);
+    }
+
     /* 2. 在子进程中调用 **`setsid()`** 创建新会话 */
     pid = setsid();
     {
-        perror("setsid()");
-        exit(1);
+        syslog(LOG_ERR, "setsid() %s", strerror(errno));
+        return -1;
     }
 
     /* 3. 改变工作目录到根目录，防止占用可卸载的文件系统 */
-    /* 4. 重定向标准输入、输出和错误到 **`/dev/null`** 或其他地方 */
-    /* 5. 设置文件创建掩码（**`umask()`**）为合适的值 */
-    /* 6. 执行守护进程的工作任务 */
+    if (chdir("/") < 0)
+    {
+        syslog(LOG_ERR, "chdir() %s", strerror(errno));
+        return -1;
+    }
 
+    /* 5. 设置文件创建掩码（**`umask()`**）为合适的值 */
+    umask(0);
+}
+
+static int socket_init()
+{
+    int sd;
+    struct ip_mreqn mreq;
+
+    sd = socket();
+    if (sd < 0)
+    {
+        syslog(LOG_ERR, "socket() %s\n", strerror(errno));
+        return -1;
+    }
+
+    inet_pton(AF_INET, server_conf.multicast_group, &mreq.imr_multiaddr);
+    inet_pton(AF_INET, "0.0.0.0", &mreq.imr_address);
+    mreq.imr_ifindex = if_indextoname(server_conf.interface);
+    if (setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, &mreq, sizeof(mreq)) < 0)
+    {
+        syslog(LOG_ERR, "setsockept() %s\n", strerror(errno));
+        return -1;
+    }
+
+    // bind
+
+    close(sd);
 }
 
 int main(int argc, char *argv[])
 {
+    openlog("netredio", LOG_PID, LOG_DAEMON|LOG_SYSLOG);
+
     /* 解析命令行参数，提供默认值 */
     parse_command_line_options(argc, argv);
 
+    setup_signals();
+
     /* 将进程设置为守护进程 */
-    demonize();
+    if (demonize() < 0)
+    {
+        syslog(LOG_ERR, "demonize() error.");
+        exit(1);
+    }
+
+    while (1)
+        ;
+
     /* 初始化 `socket`，设置属性 */
+    socket_init();
+
     /* 获取频道信息，创建节目单和频道线程 */
     /* 主进程保持运行，使用 `pause` 等待 */
 
-    exit(0);
 }
